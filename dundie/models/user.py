6813e6c0aec44a30afa8890751954bcac3db5c88
KauaEdu11 DO Santos <Kauae3897@gmail.com>
2024-09-15 20:20:31 +0000

    Da parte 13 até a parte 17 Obs: Alguns arquivos nao fazem parte da parte 13 a parte 17, acabei dando commit errado, sorry

---
+++ dundie/models/user.py
@@ -0,0 +1,31 @@
+"""User related data model"""
+from typing import Optional
+from sqlmodel import Field, SQLModel
+from dundie.security import HashedPassword
+
+
+class User(SQLModel, table=True):
+    """Represents the User Model"""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    email: str = Field(unique=True, nullable=False)
+    username: str = Field(unique=True, nullable=False)
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
+    password: HashedPassword
+    name: str = Field(nullable=False)
+    dept: str = Field(nullable=False)
+    currency: str = Field(nullable=False)
+
+    @property
+    def superuser(self):
+        """"Users belonging to management dept are admins."""
+        return self.dept == "management"
+
+
+def generate_username(name: str) -> str:
+    """Generate a slug from user.name,
+    "Kauã Eduardo" -> "kauã-eduardo"
+    # TODO: library slugfy
+    """
+    return name.lower().replace(" ", "-")
